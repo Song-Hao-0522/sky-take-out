63631e728afe36e6e777255457254b440f5c14bb
Hao Song <1505027603@qq.com>
2026-06-06 01:59:45 +0800

    新增菜品业务开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,49 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Slf4j
+@Api("通用接口")
+public class CommonController {
+    private final AliOssUtil aliOssUtil;
+
+    public CommonController(AliOssUtil aliOssUtil) {
+        this.aliOssUtil = aliOssUtil;
+    }
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @ApiOperation("文件上传")
+    @PostMapping("/upload")
+    public Result<String> upload(@RequestBody MultipartFile file) throws IOException {
+        log.info("文件上传：{}",file);
+        String originalFilename = file.getOriginalFilename();
+        String extension = null;
+        if (originalFilename != null) {
+            extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        }else {
+            throw new RuntimeException("文件未上传");
+        }
+        String objectName = UUID.randomUUID() + extension;
+        String upload = aliOssUtil.upload(file.getBytes(), objectName);
+        log.info("文件上传成功，返回路径：{}", upload);
+        return Result.success(upload);
+    }
+}
